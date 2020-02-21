c98ca519eff225fd3acfb9a95253a1ebf399af8a
oussemajaberi <39085319+oussemajaberi@users.noreply.github.com>
2020-02-22 00:35:43 +0100

    badelt lmenu wzedt page qui somme nous lezemni mail mteehom bch nzid google analytics

--- includes/header.php
+++ includes/header.php
@@ -5,45 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Arinas</title>
+    <link rel="shortcut icon" type="image/png" href="./images/logoarinas2.png">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:600&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="icon/css/all.min.css">   
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style1.css">
 </head>
 <body>
-<div class="container">
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <a class="navbar-brand" href="#"><img src="./images/logoo.png" width="75rem"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Qui sommes nous</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Nos produit</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Blog</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
-    <form class="recherche">
-      <a href="#"><i class="fas fa-search"></i></a>
-    </form>
-  </div>
-</nav>
+<div class="menu" style="box-shadow: 0 0 5px 0 #ccc">
+   <div class="logo"><img src="./images/logoarinas2.png" width=120rem style=margin-top:10px></div>
+   <nav class="nav" >
+      <ul >
+        <li><a style="color:#333" href="home.php">Acceuil</a></li>
+        <li><a style="color:#333">Produit</a></li>
+        <li><a  style="color:#333" href="qsn.php">Qui sommes nous</a></li>
+        <li><a  style="color:#333">Blog</a></li>
+        <li><a  style="color:#333">Contact</a></li>
+        <li><a  style="color:#333"><i class="fa fa-search"></i></a></li>
+      </ul>
+   </nav>
 </div>
+  </div>
+  
+
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
-$(function(){
-    'use strict';
-    var winH= $(window).height(),
-        navH=$('.navbar').innerHeight();
+$(document).ready(function(){
+    $(".menuu").mouseenter(function(){
+      $(this).css({"background-color": "rgba(255,255,255,0.8)"});
+      $(".nav a").css({"color": "#333"});
+      $(".logo img").attr("src", "./images/logoarinas2.png");
 
-    $('.slider').height(winH-navH);
+      $(".menuu").mouseleave(function(){
+        $(this).css({"background-color": ""});
+        $(".nav a").css({"color": "#fff"});
+        $(".logo img").attr("src", "./images/arinaswhite.png");
+
+        
+            $(window).scroll(function(){
+               var cuurentposition=$(this).scrollTop();
+               if (cuurentposition>=1){
+                $(".menuu").css({"background-color": "rgba(255,255,255,0.8)"});
+                $(".nav a").css({"color": "#333"});
+                $(".logo img").attr("src", "./images/logoarinas2.png");
+               
+            
+             
+
+               }
+               else{
+                $(".menuu").css({"background-color": ""});
+                $(".nav a").css({"color": "#fff"});
+                $(".logo img").attr("src", "./images/arinaswhite.png");
+                
+               }
+                
+              }
+        
+          );
+  
 
 
-} )
+      
+    })
+    });
+
+    $(".overlay1").mouseenter(function(){
+      $(".btnava").css({"visibility": "visible"});
+    });
+
+    $(".overlay1").mouseleave(function(){
+      $(".btnava").css({"visibility": "hidden"});
+    });
+  });
